21201458e14e00485c752e8c992b4b3715cc22d0
Ricardo Sena <ricardoxsena@gmail.com>
2020-10-06 20:29:44 -0300

    new animal images
    Added a method (fox) that search random fox images (from imgur) and display for us.

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def get_url():
     url = contents['url']
     return url
 
+def get_fox_url():
+    contents = requests.get('https://some-random-api.ml/img/fox').json()
+    link = contents['link']
+    return link
+
 def get_image_url():
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
@@ -23,9 +28,23 @@ def get_image_url():
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 
+def get_fox_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_fox_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 @bot.message_handler(commands=['bop'])
 def bop(message):
     url = get_image_url()
+    chat_id = message.chat.id
+    bot.send_photo(chat_id,url)
+    
+@bot.message_handler(commands=['bop'])
+def fox(message):
+    url = get_fox_image_url()
     chat_id = message.chat.id
     bot.send_photo(chat_id,url)
     
