dc1b49a8455e915e0af4c8e8a02b64b47214f587
luana <luludsf@gmail.com>
2020-10-07 20:08:40 -0300

    atualizando metodos e removendo codigo duplicado

--- main.py
+++ main.py
@@ -4,72 +4,43 @@ import re
 from telebot import types
 
 
-bot = telebot.TeleBot('856966669:AAF4IAZNea21VgxQyjsi4sexqxHdnPs3898')
+bot = telebot.TeleBot('bot token')
 
 @bot.message_handler(commands=['start'])
 def chart(message):
     bot.send_message(message.chat.id, "OI")
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
+def get_url(url,key):
+    contents = requests.get(url).json()
+    url = contents[key]
+    return url    
 
-def get_fox_url():
-    contents = requests.get('https://some-random-api.ml/img/fox').json()
-    link = contents['link']
-    return link
-
-
-
-def get_koala_url():
-    contents = requests.get('https://some-random-api.ml/img/koala').json()
-    link = contents['link']
-    return link
-
-def get_image_url():
+def get_image_url(url,key):
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
     while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
-    return url
-
-def get_fox_image_url():
-    allowed_extension = ['jpg','jpeg','png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_fox_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
-    return url
-
-
-def get_koala_image_url():
-    allowed_extension = ['jpg','jpeg','png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_koala_url()
+        url = get_url(url,key)
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 
 @bot.message_handler(commands=['bop'])
 def bop(message):
-    url = get_image_url()
+    url = get_image_url('https://random.dog/woof.json','url')
     chat_id = message.chat.id
     bot.send_photo(chat_id,url)
     
 @bot.message_handler(commands=['fox'])
 def fox(message):
-    url = get_fox_image_url()
+    url = get_image_url('https://some-random-api.ml/img/fox','link')
     chat_id = message.chat.id
     bot.send_photo(chat_id,url)
 
 @bot.message_handler(commands=['koala'])
-def koala(message):
-    url = get_koala_image_url()
+def fox(message):
+    url = get_image_url('https://some-random-api.ml/img/koala','link')
     chat_id = message.chat.id
     bot.send_photo(chat_id,url)
-    
+
 
 bot.polling()
 
