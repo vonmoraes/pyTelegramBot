b1976029b4936292b0ed21739315fc4cb04b0be7
Luana Duarte <luludsf@gmail.com>
2020-10-07 18:47:25 -0300

    cleaning the code
    and adding a new feature (fox images)

--- main.py
+++ main.py
@@ -4,34 +4,36 @@ import re
 from telebot import types
 
 
-bot = telebot.TeleBot('856966669:AAF4IAZNea21VgxQyjsi4sexqxHdnPs3898')
+bot = telebot.TeleBot('Insira o token do seu bot')
 
 @bot.message_handler(commands=['start'])
 def chart(message):
     bot.send_message(message.chat.id, "OI")
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
+def get_url(url,key):
+    contents = requests.get(url).json()
+    url = contents[key]
+    return url    
 
-def get_image_url():
+def get_image_url(url,key):
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
     while file_extension not in allowed_extension:
-        url = get_url()
+        url = get_url(url,key)
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 
 @bot.message_handler(commands=['bop'])
 def bop(message):
-    url = get_image_url()
+    url = get_image_url('https://random.dog/woof.json','url')
+    chat_id = message.chat.id
+    bot.send_photo(chat_id,url)
+    
+@bot.message_handler(commands=['fox'])
+def fox(message):
+    url = get_image_url('https://some-random-api.ml/img/fox','link')
     chat_id = message.chat.id
     bot.send_photo(chat_id,url)
     
 
 bot.polling()
-
-
-
-
